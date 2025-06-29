29a69222c363a5730a5c0a6599f43cb00a8aef48
Emrah Isovic <emrahisovic20@gmail.com>
2025-06-29 18:05:34 +0200

    Added custom inputs and submit button components

---
+++ src/components/CustomInput.jsx
@@ -0,0 +1,47 @@
+function CustomInput({
+  id,
+  title,
+  type,
+  label,
+  required,
+  placeholder,
+  onInputChangeHandler,
+}) {
+  return (
+    <div className='flex items-start flex-col'>
+      <label
+        htmlFor={id}
+        className='text-sm font-medium tracking-wide text-gray-600 dark:text-gray-300 mb-2'
+      >
+        {label}
+        <span className='text-amber-600 dark:text-amber-400'>
+          {required ? ' (required)' : ''}
+        </span>
+      </label>
+      {type === 'textarea' ? (
+        <textarea
+          id={id}
+          name={title}
+          required={required}
+          placeholder={placeholder}
+          rows='5'
+          className='border-1 border-slate-200 dark:border-gray-600 font-regular placeholder:font-medium font-base tracking-wide rounded-md px-3 py-2 focus:outline-amber-500 dark:text-gray-50 text-gray-900 placeholder:text-gray-400 inline-block w-full mb-5'
+          onChange={onInputChangeHandler}
+        />
+      ) : (
+        <input
+          type={type}
+          id={id}
+          name={title}
+          required={required}
+          placeholder={placeholder}
+          className='border-1 border-slate-200 dark:border-gray-600 font-regular placeholder:font-medium font-base tracking-wide rounded-md px-3 py-2 focus:outline-amber-500 dark:scheme-dark dark:text-gray-50 text-gray-900 placeholder:text-gray-400 inline-block w-full mb-5'
+          autoComplete='off'
+          onChange={onInputChangeHandler}
+        />
+      )}
+    </div>
+  );
+}
+
+export default CustomInput;
---
+++ src/components/FormSubmitButton.jsx
@@ -0,0 +1,14 @@
+function FormSubmitButton() {
+  return (
+    <div>
+      <button
+        className='font-semibold tracking-wide bg-amber-500 px-5 py-2.5 rounded-xl focus:outline-black text-gray-50 cursor-pointer'
+        type='submit'
+      >
+        Submit
+      </button>
+    </div>
+  );
+}
+
+export default FormSubmitButton;
