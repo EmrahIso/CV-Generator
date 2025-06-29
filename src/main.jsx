a0ca08a441a8f586e07ebf7689fe9c06d031e158
Emrah Isovic <emrahisovic20@gmail.com>
2025-06-29 18:06:50 +0200

    Imported css into react and added Tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import '@fontsource/inter';
+import './index.css';
+import App from './components/App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+/*
+
+CV Generator 
+
+- Header naigacija 
+- Forma za unos podataka
+- Navigacija za stajling
+- Footer info
+
+- Resume preview (edit i submit buttoni)
+
+- Final Resume
+
+Vrste Formi:
+
+  - general information
+
+  - contact information
+
+
+  - skills
+  - education  
+  
+  - practical experience
+  
+
+*/
