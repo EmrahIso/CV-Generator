77087a0c9abc1221824411d9665c7af28dc80603
Emrah Isovic <emrahisovic20@gmail.com>
2025-07-12 21:01:50 +0200

    feat: header and footer navigation with new default CV form data

--- src/components/HeaderNavigation.jsx
+++ src/components/HeaderNavigation.jsx
@@ -1,16 +1,19 @@
-import ThemeButton from './ThemeButton';
+import ThemeButton from './ThemeButton.jsx';
 
 function HeaderNavigation({ colorTheme, changeColorThemeHandler }) {
   return (
     <header>
-      <div className='px-3 py-3 rounded-xl bg-[white] dark:bg-gray-800 flex items-center justify-between'>
-        <h1 className='text-xl font-semibold tracking-wide text-neutral-900 dark:text-neutral-50'>
-          <span className='text-amber-600 dark:text-amber-400'>CV</span>ision
-        </h1>
-        <ThemeButton
-          colorTheme={colorTheme}
-          changeColorThemeHandler={changeColorThemeHandler}
-        />
+      <div className='px-3 py-3 rounded-xl bg-[white] dark:bg-gray-800'>
+        <nav className='flex items-center justify-between'>
+          <h1 className='text-2xl font-semibold tracking-wide text-neutral-900 dark:text-neutral-50'>
+            <span className='text-orange-700 dark:text-orange-600'>CV</span>
+            ision
+          </h1>
+          <ThemeButton
+            colorTheme={colorTheme}
+            changeColorThemeHandler={changeColorThemeHandler}
+          />
+        </nav>
       </div>
     </header>
   );
